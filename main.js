fe410b53ed50c4adf7ae43917481723b19a7179c
prranesh-arch <81365988+prranesh-arch@users.noreply.github.com>
2021-07-24 17:59:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_name="Ranbir";
+var last_name="Kapoor";
+var a=10;
+var b=5;
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var addr=a+b;
+console.log(addr);
+
+var sub=a-b;
+console.log(sub);
+
+var mul=a*b;
+console.log(mul);
+
+var div=a/b;
+console.log(div);
